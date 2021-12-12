dca12de900e2971043ea3d08d9f4fb126933d36e
Agustin Diaz <agusjd92@gmail.com>
2021-12-12 19:56:22 -0300

    desafio2

--- App.js
+++ App.js
@@ -1,12 +1,53 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React,{useState} from 'react';
+import { StyleSheet, Text, View,TextInput,Button } from 'react-native';
 
 export default function App() {
+  const [list,setList] = useState([])
+  const [textInput,setTextinput] = useState("")
+
+  const onAdd = () =>{
+    setList([... list,
+      {id:Math.random().toString(), value:textInput}])
+  }
+
+  const onDelete = (id) =>{
+    setList(list.filter(i => i.id != id))
+  }
+  console.log(list)
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text
+      style={styles.title}>Lista de compras</Text>
+      <View>
+          <TextInput
+          placeholder="Ingrese un elemento"
+          style={styles.input}
+          onChangeText={(text)=> setTextinput(text)}
+          />
+         
+          <Button
+          title="Agregar"
+          color="darkslategrey"
+          onPress={()=> onAdd()} />
+      </View>
+      <View>
+        {
+          list.length > 0 ? list.map(i=>
+            <View style={{flexDirection:"row",marginTop:"1rem"}}>
+              <Text style={{color:"white",fontWeight:"bold",fontSize:18}}> {i.value} </Text>
+              <Button
+              color="darkslategrey"
+              title="X"
+              onPress={()=> onDelete(i.id)}
+              ></Button>
+            </View>)
+            :
+            <Text style={{marginTop:"1rem",fontWeight:"bold",color:"white",fontSize:18}}>No hay Tareas</Text>
+        }
+
+      </View>
+   
     </View>
   );
 }
@@ -14,8 +55,23 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'green',
     alignItems: 'center',
     justifyContent: 'center',
+
   },
+  title:{
+    fontWeight:"bold",
+    color:"white"
+  },
+  input:{
+    backgroundColor:"white",
+    marginTop:"1rem",
+    marginBottom:"1rem",
+    height: 25
+  },
+  button:{
+    height: 21
+  }
+
 });
